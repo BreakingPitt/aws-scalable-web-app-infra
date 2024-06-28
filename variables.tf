e61fa003a0fae97211ee33c6eca55dd3318abfe4
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-06-28 10:05:53 +0200

    Reorder variables and variable parameter definition

--- variables.tf
+++ variables.tf
@@ -1,14 +1,13 @@
+variable "availability_zones" {
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  description = "List of availability zones"
+  type        = list(string)
+}
+
 variable "cidr_block" {
   default     = "10.0.1.0"
   description = "The IP prefix to the CIDR block assigned to the VPC"
   type        = string
-}
-
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  type        = list(string)
-  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
 }
 
 variable "s3_bucket_arn" {
