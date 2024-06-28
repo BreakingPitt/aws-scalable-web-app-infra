87126e1a9f3fe8fd6fa98f3c3e8dcb563e361fae
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-06-28 13:13:40 +0200

    Add Terraform resource to add LB security group

--- main.tf
+++ main.tf
@@ -30,6 +30,26 @@ resource "aws_route_table_association" "aws_scalable_web_demo_route_private_tabl
   route_table_id = aws_route_table.aws_scalable_web_demo_route_table.id
 }
 
+resource "aws_security_group" "aws_scalable_web_demo_route_load_balancer_sg" {
+  name        = "aws_scalable_web_demo_route_load_balancer_sg"
+  description = "Allow traffic to 80 port from the Internet"
+  vpc_id      = aws_vpc.aws_scalable_web_demo_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_subnet" "aws_scalable_web_demo_public_subnets" {
   count                    = 3
   cidr_block               = cidrsubnet(aws_vpc.aws_scalable_web_demo_vpc.cidr_block, 4, count.index)
