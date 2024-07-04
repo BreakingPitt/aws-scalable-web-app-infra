59b22db89d0bb083ce9b955a5deff5a722213e03
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-07-04 08:17:48 +0200

    Add KMS Key resources

--- main.tf
+++ main.tf
@@ -9,7 +9,39 @@ resource "aws_internet_gateway" "aws_scalable_web_demo_internet_gateway" {
   vpc_id = aws_vpc.aws_scalable_web_demo_vpc.id
 }
 
-resource "aws_route_table" "aws_scalable_web_demo_route_table" {
+resource "aws_kms_alias" "s3_kms_key_alias" {
+  name          = "alias/aws-scalable-web-demo-kms-key-1"
+  target_key_id = aws_kms_key.aws_scalable_web_demo_kms_key.id
+}
+
+resource "aws_kms_key" "aws_scalable_web_demo_kms_key" {
+  description             = "KMS key for S3 server-side encryption"
+  deletion_window_in_days = 30
+}
+
+resource "aws_kms_key_policy" "aws_scalable_web_demo_kms_key_policy" {
+  key_id = aws_kms_key.aws_scalable_web_demo_kms_key.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "aws-scalable-web-demo-kms-key-1",
+  "Statement": [
+    {
+      "Sid": "Enable IAM User Permissions",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action": "kms:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_scalable_web_demo_route_table" {
   vpc_id = aws_vpc.aws_scalable_web_demo_vpc.id
 
   route {
