3e06bbfbc00df651370133f730aa3aa2c25dd0ea
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-07-04 10:03:59 +0200

    Define EC2 instance type variable.

--- variables.tf
+++ variables.tf
@@ -1,17 +1,23 @@
 variable "availability_zones" {
   default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  description = "List of availability zones"
+  description = "List of availability zones."
   type        = list(string)
 }
 
 variable "cidr_block" {
   default     = "10.0.1.0"
-  description = "The IP prefix to the CIDR block assigned to the VPC"
+  description = "The IP prefix to the CIDR block assigned to the VPC."
+  type        = string
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "The instance type to use."
   type        = string
 }
 
 variable "s3_bucket_name" {
   default     = "aws_scalable_web_demo_s3_flow_logs"
-  description = "The name  of the S3 bucket for flow logs"
+  description = "The name  of the S3 bucket for flow logs."
   type        = string
 }
