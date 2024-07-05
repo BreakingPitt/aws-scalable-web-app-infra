a055a33c133ab54c2dfae9ae1d94de288822edff
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-07-05 10:49:09 +0200

    Reorder output parameters

--- outputs.tf
+++ outputs.tf
@@ -4,26 +4,26 @@ output "elb_dns_name" {
 }
 
 output "internet_gateway_id" {
-  value       = aws_internet_gateway.aws_scalable_web_demo_internet_gateway.id
   description = "The Id of the Internet Gateway."
+  value       = aws_internet_gateway.aws_scalable_web_demo_internet_gateway.id
 }
 
 output "private_subnet_id" {
-  value       = aws_subnet.aws_scalable_web_demo_private_subnets[*].id
   description = "Subnet Id"
+  value       = aws_subnet.aws_scalable_web_demo_private_subnets[*].id
 }
 
 output "public_subnet_id" {
-  value       = aws_subnet.aws_scalable_web_demo_public_subnets[*].id
   description = "Subnet Id"
+  value       = aws_subnet.aws_scalable_web_demo_public_subnets[*].id
 }
 
 output "vpc_cidr_block" {
-  value       = aws_vpc.aws_scalable_web_demo_vpc.cidr_block
   description = "The CIDR block of the VPC."
+  value       = aws_vpc.aws_scalable_web_demo_vpc.cidr_block
 }
 
 output "vpc_id" {
-  value       = aws_vpc.aws_scalable_web_demo_vpc.id
   description = "VPC Id"
+  value       = aws_vpc.aws_scalable_web_demo_vpc.id
 }
