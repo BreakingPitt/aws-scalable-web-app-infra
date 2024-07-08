aa440472ab32b075932fdcf663e2ed41747d2bf7
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-07-08 14:58:20 +0200

    Add data resource to find latest Amazon Linux 2 AMI

--- data.tf
+++ data.tf
@@ -1,2 +1,17 @@
 data "aws_caller_identity" "current" {}
 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
